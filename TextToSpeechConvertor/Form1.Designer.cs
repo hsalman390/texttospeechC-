479693ef715dc58c221a9a92a768b45877180d78
hsalman390 <hsalman390@gmail.com>
2022-09-22 11:11:55 +0500

    first commit

---
+++ TextToSpeechConvertor/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace TextToSpeechConvertor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textbox = new System.Windows.Forms.RichTextBox();
+            this.speakbtn = new System.Windows.Forms.Button();
+            this.stopbtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textbox
+            // 
+            this.textbox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textbox.Location = new System.Drawing.Point(0, 0);
+            this.textbox.Name = "textbox";
+            this.textbox.Size = new System.Drawing.Size(800, 250);
+            this.textbox.TabIndex = 0;
+            this.textbox.Text = "";
+            // 
+            // speakbtn
+            // 
+            this.speakbtn.Location = new System.Drawing.Point(212, 280);
+            this.speakbtn.Name = "speakbtn";
+            this.speakbtn.Size = new System.Drawing.Size(109, 48);
+            this.speakbtn.TabIndex = 1;
+            this.speakbtn.Text = "Speak";
+            this.speakbtn.UseVisualStyleBackColor = true;
+            this.speakbtn.Click += new System.EventHandler(this.speakbtn_Click);
+            // 
+            // stopbtn
+            // 
+            this.stopbtn.Location = new System.Drawing.Point(404, 280);
+            this.stopbtn.Name = "stopbtn";
+            this.stopbtn.Size = new System.Drawing.Size(105, 48);
+            this.stopbtn.TabIndex = 2;
+            this.stopbtn.Text = "Stop";
+            this.stopbtn.UseVisualStyleBackColor = true;
+            this.stopbtn.Click += new System.EventHandler(this.stopbtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.stopbtn);
+            this.Controls.Add(this.speakbtn);
+            this.Controls.Add(this.textbox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox textbox;
+        private System.Windows.Forms.Button speakbtn;
+        private System.Windows.Forms.Button stopbtn;
+    }
+}
+
---
+++ TextToSpeechConvertor/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech;
+using System.Speech.Synthesis;
+
+namespace TextToSpeechConvertor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SpeechSynthesizer speechSynthesizerObj;
+        private void speakbtn_Click(object sender, EventArgs e)
+        {
+            //Disposes the SpeechSynthesizer object  
+            if (textbox.Text != "")
+            {
+                speechSynthesizerObj = new SpeechSynthesizer();
+                //Asynchronously speaks the contents present in RichTextBox1   
+                speechSynthesizerObj.SpeakAsync(textbox.Text);
+                stopbtn.Enabled = true;
+            }
+        }
+
+        private void stopbtn_Click(object sender, EventArgs e)
+        {
+            if (speechSynthesizerObj != null)
+            {
+                //Disposes the SpeechSynthesizer object   
+                speechSynthesizerObj.Dispose();
+                speakbtn.Enabled = true;
+                stopbtn.Enabled = false;
+            }
+        }
+    }
+}
